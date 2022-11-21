f0999f40f68f1a76f39a109bdafa9f77669d7d73
geyachowdary <30752608+geyachowdary@users.noreply.github.com>
2022-11-20 20:41:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,29 +13,22 @@ function testStrength() {
         document.getElementById('password-strength').className = "invalid";
         setProgressBar(5, "invalid");
     }
-    // else if (password.length >= 16) {
-    //     password_strength = "Excellent";
-    //     document.getElementById('password-strength').className = "excellent";
-    //     setProgressBar(100, "excellent");
-    // }
-    // else if (password.length >= MIN_LENGTH && hasUppercase(password) && hasLowerCase(password)) {
-  else{  if (hasSpecial(password) && hasNum(password) && hasUppercase(password) && hasLowerCase(password) && password.length >= MIN_LENGTH && password.length < 16) {
-        password_strength = "Good";
-        document.getElementById('password-strength').className = "good";
-        setProgressBar(80, "good");
-    }
-    else if (hasSpecial(password) && hasNum(password) && hasUppercase(password) && hasLowerCase(password) && password.length >= 16) {
+    else if (password.length >= 16) {
         password_strength = "Excellent";
         document.getElementById('password-strength').className = "excellent";
         setProgressBar(100, "excellent");
     }
-    else {
-        password_strength = "Fair";
-        document.getElementById('password-strength').className = "fair";
-        setProgressBar(50, "fair");
+    else if (password.length >= MIN_LENGTH && hasUppercase(password) && hasLowerCase(password)) {
+        if (hasSpecial(password) && hasNum(password)) {
+            password_strength = "Good";
+            document.getElementById('password-strength').className = "good"; 
+            setProgressBar(80, "good");
+        } else {
+            password_strength = "Fair";
+            document.getElementById('password-strength').className = "fair";
+            setProgressBar(50, "fair");
+        }
     }
-    // }
-}
 
     // Display password strength
     document.getElementById('password-strength').innerHTML = password_strength;
@@ -60,7 +53,7 @@ function hasNum(password) {
 
 // Pass in what percentage of the bar should be filled and what the class should be
 function setProgressBar(percent, className) {
-    size = (percent / 5)
+    size = (percent/5)
     td = document.body.getElementsByTagName("td");
     console.log(td);
     for (i = 0; i < size; i++) {
