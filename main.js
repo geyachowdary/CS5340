caf59f13991942e0bc980be1d752a6d910f1196a
geyachowdary <30752608+geyachowdary@users.noreply.github.com>
2022-11-20 20:39:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+MIN_LENGTH = 8;
+
+function testStrength() {
+    // Get Password
+    password = document.getElementById('password').value;
+    console.log(password);
+    password_strength = "";
+    setProgressBar(100, "neutral");
+
+    // Check if it's too short
+    if (password.length < MIN_LENGTH) {
+        password_strength = "Too short";
+        document.getElementById('password-strength').className = "invalid";
+        setProgressBar(5, "invalid");
+    }
+    // else if (password.length >= 16) {
+    //     password_strength = "Excellent";
+    //     document.getElementById('password-strength').className = "excellent";
+    //     setProgressBar(100, "excellent");
+    // }
+    // else if (password.length >= MIN_LENGTH && hasUppercase(password) && hasLowerCase(password)) {
+  else{  if (hasSpecial(password) && hasNum(password) && hasUppercase(password) && hasLowerCase(password) && password.length >= MIN_LENGTH && password.length < 16) {
+        password_strength = "Good";
+        document.getElementById('password-strength').className = "good";
+        setProgressBar(80, "good");
+    }
+    else if (hasSpecial(password) && hasNum(password) && hasUppercase(password) && hasLowerCase(password) && password.length >= 16) {
+        password_strength = "Excellent";
+        document.getElementById('password-strength').className = "excellent";
+        setProgressBar(100, "excellent");
+    }
+    else {
+        password_strength = "Fair";
+        document.getElementById('password-strength').className = "fair";
+        setProgressBar(50, "fair");
+    }
+    // }
+}
+
+    // Display password strength
+    document.getElementById('password-strength').innerHTML = password_strength;
+}
+
+
+function hasLowerCase(password) {
+    return (/[a-z]/.test(password));
+}
+
+function hasUppercase(password) {
+    return (/[A-Z]/.test(password));
+}
+
+function hasSpecial(password) {
+    return (/\W|_/g.test(password))
+}
+
+function hasNum(password) {
+    return (/\d/.test(password));
+}
+
+// Pass in what percentage of the bar should be filled and what the class should be
+function setProgressBar(percent, className) {
+    size = (percent / 5)
+    td = document.body.getElementsByTagName("td");
+    console.log(td);
+    for (i = 0; i < size; i++) {
+        td[i].className = className;
+    }
+}
---
+++ test.js
@@ -0,0 +1,110 @@
+MIN_LENGTH = 8;
+
+function testStrength() {
+    // Get Password
+    password = document.getElementById('password').value;
+    console.log(password);
+    password_strength = "";
+    setProgressBar(100, "neutral");
+    if (password.length < MIN_LENGTH) {
+        setStatus("invalid");
+    }
+    else {
+        isPasswordCompromised(password).then(isCompromised => {
+            console.log('Is compromised?', isCompromised)
+            var strength = 0;
+            strength += /[A-Z]+/.test(password) ? 1 : 0;
+            strength += /[a-z]+/.test(password) ? 1 : 0;
+            strength += /[0-9]+/.test(password) ? 1 : 0;
+            strength += /[\W]+/.test(password) ? 1 : 0;
+
+            switch (strength) {
+                case 1:
+                    setStatus("poor");
+                    break;
+                case 2:
+                    if (isCompromised) {
+                        setStatus("poor");
+                    }
+                    else {
+                        setStatus("good");
+                    }
+                    break;
+                case 3:
+                    if (isCompromised) {
+                        setStatus("poor");
+                    }
+                    else {
+                        setStatus("good");
+                    }
+                    break;
+                case 4:
+                    if (isCompromised) {
+                        setStatus("good")
+                    }
+                    else {
+                        setStatus("excellent");
+                    }
+
+            }
+            // Display password strength
+        });
+
+    }
+
+
+}
+
+function setStatus(status) {
+
+    if (status.toLowerCase() == "poor") {
+        password_strength = "poor";
+        document.getElementById('password-strength').className = "fair";
+        setProgressBar(50, "fair");
+    }
+    if (status.toLowerCase() == "good") {
+        password_strength = "Good";
+        document.getElementById('password-strength').className = "Good";
+        setProgressBar(80, "fair");
+    }
+    if (status.toLowerCase() == "excellent") {
+        password_strength = "Excellent";
+        document.getElementById('password-strength').className = "excellent";
+        setProgressBar(100, "excellent");
+    }
+    if (status.toLowerCase() == "invalid") {
+        password_strength = "Too short";
+        document.getElementById('password-strength').className = "invalid";
+        setProgressBar(5, "invalid");
+    }
+
+    document.getElementById('password-strength').innerHTML = password_strength;
+
+}
+
+
+function hasLowerCase(password) {
+    return (/[a-z]/.test(password));
+}
+
+function hasUppercase(password) {
+    return (/[A-Z]/.test(password));
+}
+
+function hasSpecial(password) {
+    return (/\W|_/g.test(password))
+}
+
+function hasNum(password) {
+    return (/\d/.test(password));
+}
+
+// Pass in what percentage of the bar should be filled and what the class should be
+function setProgressBar(percent, className) {
+    size = (percent / 5)
+    td = document.body.getElementsByTagName("td");
+    console.log(td);
+    for (i = 0; i < size; i++) {
+        td[i].className = className;
+    }
+}
